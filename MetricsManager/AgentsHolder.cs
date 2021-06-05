a6c7565e33e6c8bedd1e09cd4952cd9caa4eb93d
Kolodinsky <Photon.74@gmail.com>
2021-06-05 19:57:53 +0300

    MetricsManager

---
+++ MetricsManager/AgentsHolder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MetricsManager
+{
+    public class AgentsHolder
+    {
+        public List<AgentInfo> Agents;
+
+        public AgentsHolder()
+        {
+            Agents = new List<AgentInfo>();
+        }
+    }
+}
---
+++ MetricsManager/Controllers/AgentsController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MetricsManager.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AgentsController : ControllerBase
+    {
+        private readonly AgentsHolder _holder;
+
+        public AgentsController(AgentsHolder holder)
+        {
+            _holder = holder;
+        }
+
+        [HttpPost("register")]
+        public IActionResult RegisterAgent([FromBody] AgentInfo agentInfo)
+        {
+            return Ok();
+        }
+
+        [HttpPut("enable/{agentId}")]
+        public IActionResult EnableAgentById([FromRoute] int agentId)
+        {
+            return Ok();
+        }
+
+        [HttpPut("disable/{agentId}")]
+        public IActionResult DisableAgentById([FromRoute] int agentId)
+        {
+            return Ok();
+        }
+    }
+}
